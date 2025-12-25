818a35c9b7e805722d37c0295f2de39af4be0b92
Dasha <girl.is.anime20@gmail.com>
2025-12-25 22:42:48 +0300

    feat: add gRPC lossary service with sample terms

---
+++ glossary/data.py
@@ -0,0 +1,19 @@
+from glossary_pb2 import Term
+
+TERMS = {
+    "grpc": Term(
+        term="gRPC",
+        definition="Фреймворк удалённого вызова процедур от Google.",
+        sources=["https://grpc.io"]
+    ),
+    "protobuf": Term(
+        term="Protocol Buffers",
+        definition="Механизм сериализации структурированных данных.",
+        sources=["https://developers.google.com/protocol-buffers"]
+    ),
+    "docker": Term(
+        term="Docker",
+        definition="Платформа контейнеризации приложений.",
+        sources=["https://www.docker.com"]
+    ),
+}
